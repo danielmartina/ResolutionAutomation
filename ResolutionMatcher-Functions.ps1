eb95adb62a3849c5137a3b1f363aa7dd25d65219
danielmartina <57731531+danielmartina@users.noreply.github.com>
2024-05-12 22:00:21 +0200

    Add DPI scaling

--- ResolutionMatcher-Functions.ps1
+++ ResolutionMatcher-Functions.ps1
@@ -126,6 +126,7 @@ function OnStreamStart($width, $height, $refresh) {
     $expectedRes = Join-Overrides -width $width -height $height -refresh $refresh
     Set-ScreenResolution -Width $expectedRes.Width -Height $expectedRes.Height -Freq $expectedRes.Refresh
     Assert-ResolutionChange -width $expectedRes.Width -height $expectedRes.Height -refresh $expectedRes.Refresh
+    Set-Scaling -scaling 4
 }
 
 function OnStreamEnd($hostResolution) {
@@ -138,9 +139,28 @@ function OnStreamEnd($hostResolution) {
         }
     }
     Set-ScreenResolution -Width $hostResolution.Width -Height $hostResolution.Height -Freq $hostResolution.Refresh   
+    Set-Scaling -scaling 0
 }
 
-    
+function Set-Scaling {
+    # Posted by IanXue-MSFT on
+    # https://learn.microsoft.com/en-us/answers/questions/197944/batch-file-or-tool-like-powertoy-to-change-the-res.html
+    # $scaling = 0 : 100% (default)
+    # $scaling = 1 : 125% 
+    # $scaling = 2 : 150% 
+    # $scaling = 3 : 175% 
+    param($scaling)
+    $source = @'
+    [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+    public static extern bool SystemParametersInfo(
+                      uint uiAction,
+                      uint uiParam,
+                      uint pvParam,
+                      uint fWinIni);
+'@
+    $apicall = Add-Type -MemberDefinition $source -Name WinAPICall -Namespace SystemParamInfo -PassThru
+    $apicall::SystemParametersInfo(0x009F, $scaling, $null, 1) | Out-Null
+}
 
 if ($terminate) {
     Stop-ResolutionMatcherScript | Out-Null
